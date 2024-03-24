b74094642e1f9cd0eecf2e8ba8b74c9d02f55039
wangguiye <wangguiye@lenovo.com>
2024-03-24 19:46:36 +0800

    切换性别时，更新界面

--- module_base/src/main/java/com/cash/profin/ya/dinero/plata/module_base/ui/BaseActivity.kt
+++ module_base/src/main/java/com/cash/profin/ya/dinero/plata/module_base/ui/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.cash.profin.ya.dinero.plata.module_base.ui
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import android.view.Window
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.cash.profin.ya.dinero.plata.module_base.R
