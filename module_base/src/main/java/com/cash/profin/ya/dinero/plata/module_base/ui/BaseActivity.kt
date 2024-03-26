ac7bc158a256a891e9d2b663b1cf13323cc97efe
wangguiye <wangguiye@lenovo.com>
2024-03-26 18:14:03 +0800

    给状态栏设置颜色

--- module_base/src/main/java/com/cash/profin/ya/dinero/plata/module_base/ui/BaseActivity.kt
+++ module_base/src/main/java/com/cash/profin/ya/dinero/plata/module_base/ui/BaseActivity.kt
@@ -1,13 +1,13 @@
 package com.cash.profin.ya.dinero.plata.module_base.ui
 
-import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.view.Window
-import androidx.annotation.RequiresApi
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.cash.profin.ya.dinero.plata.module_base.R
+
 
 abstract class BaseActivity<T:ViewBinding>:AppCompatActivity(){
     protected open lateinit var mBinding: T
@@ -16,6 +16,7 @@ abstract class BaseActivity<T:ViewBinding>:AppCompatActivity(){
         super.onCreate(savedInstanceState)
         mBinding = getViewBinding()
         setContentView(mBinding.root)
+        setStateBar()
         initView()
         initData()
     }
@@ -25,4 +26,15 @@ abstract class BaseActivity<T:ViewBinding>:AppCompatActivity(){
     abstract fun initData()
 
     abstract fun initView()
+
+    fun setStateBar(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // 对于Android 5.0及以上版本
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = getResources().getColor(R.color.pink)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 对于Android 4.4到5.0版本
+            // 这里可以通过修改系统资源来改变状态栏颜色，但这种方式较为复杂且有局限性
+        }
+    }
 }
